d94735b8c355debd81c76727bc3bd2f017f90596
Evgeniy Plyusnin <eninbox@gmail.com>
2014-03-04 14:15:16 +0600

    Скроллинг карты без подгрузки новых тайлов

--- MapView.cs
+++ MapView.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using Geographics;
 using MapVisualization.Elements;
@@ -10,13 +11,42 @@ namespace MapVisualization
 {
     public class MapView : DrawingContainer
     {
+        public static readonly DependencyProperty TileLoaderProperty = DependencyProperty.Register(
+                                                                                                   "TileLoader",
+                                                                                                   typeof (ITileLoader),
+                                                                                                   typeof (MapView),
+                                                                                                   new PropertyMetadata(
+                                                                                                       AppDataFileCacheTileLoader
+                                                                                                           .DefaultLoader));
+
+        private readonly List<MapElement> _elements = new List<MapElement>();
+        private readonly TranslateTransform _globalTransform;
+        private Point? _dragStartPoint;
+
         static MapView()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(MapView), new FrameworkPropertyMetadata(typeof(MapView)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (MapView), new FrameworkPropertyMetadata(typeof (MapView)));
         }
 
-        public static readonly DependencyProperty TileLoaderProperty = DependencyProperty.Register(
-                                                        "TileLoader", typeof (ITileLoader), typeof (MapView), new PropertyMetadata(AppDataFileCacheTileLoader.DefaultLoader));
+        public MapView()
+        {
+            TopLeftPoint = new EarthPoint(60.5515, 56.8563);
+            ZoomLevel = 13;
+
+            Point topLeftScreenCoordinate = ScreenProjector.DefaultProjector.Project(TopLeftPoint, ZoomLevel);
+            _globalTransform = new TranslateTransform(-topLeftScreenCoordinate.X, -topLeftScreenCoordinate.Y);
+
+            int x0 = OsmIndexes.GetHorizontalIndex(TopLeftPoint.Longitude, ZoomLevel);
+            int y0 = OsmIndexes.GetVerticalIndex(TopLeftPoint.Latitude, ZoomLevel);
+
+            IEnumerable<MapTileElement> tiles =
+                Enumerable.Range(x0, 4)
+                          .SelectMany(x => Enumerable.Range(y0, 4).Select(y => GetMapTile(x, y, ZoomLevel)));
+            foreach (MapTileElement tile in tiles)
+            {
+                AddElement(tile);
+            }
+        }
 
         public ITileLoader TileLoader
         {
@@ -27,46 +57,47 @@ namespace MapVisualization
         public EarthPoint TopLeftPoint { get; set; }
         public int ZoomLevel { get; set; }
 
-        public MapView()
-        {
-            TopLeftPoint = new EarthPoint(60.5515, 56.8563);
-            ZoomLevel = 13;
-
-            var topLeftScreenCoordinate = ScreenProjector.DefaultProjector.Project(TopLeftPoint, ZoomLevel);
-            _globalTransform = new TranslateTransform(-topLeftScreenCoordinate.X, -topLeftScreenCoordinate.Y);
-
-            int x0 = OsmIndexes.GetHorizontalIndex(TopLeftPoint.Longitude, ZoomLevel);
-            int y0 = OsmIndexes.GetVerticalIndex(TopLeftPoint.Latitude, ZoomLevel);
-
-            var tiles = Enumerable.Range(x0, 4).SelectMany(x => Enumerable.Range(y0, 4).Select(y => GetMapTile(x, y, ZoomLevel)));
-            foreach (var tile in tiles)
-            {
-                AddElement(tile);
-            }
-        }
-
         private MapTileElement GetMapTile(int x, int y, int zoom)
         {
-            var image = TileLoader.GetTile(x, y, zoom);
+            ImageSource image = TileLoader.GetTile(x, y, zoom);
             return new MapTileElement(image, x, y, zoom);
         }
-
-        private readonly List<MapElement> _elements = new List<MapElement>();
 
         private void AddElement(MapElement Element)
         {
             _elements.Add(Element);
-            var visual = Element.GetVisual(ZoomLevel);
+            MapVisual visual = Element.GetVisual(ZoomLevel);
             visual.Transform = _globalTransform;
             AddVisual(visual);
         }
 
-        private readonly TranslateTransform _globalTransform;
+        #region Скроллинг карты
+
+        private void Move(Vector delta) { Move(delta.X, delta.Y); }
 
         public void Move(double dx, double dy)
         {
             _globalTransform.X += dx;
             _globalTransform.Y += dy;
         }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            _dragStartPoint = e.GetPosition(this);
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point dragCurrentPoint = e.GetPosition(this);
+                if (_dragStartPoint != null) Move(dragCurrentPoint - _dragStartPoint.Value);
+                _dragStartPoint = dragCurrentPoint;
+            }
+            base.OnMouseMove(e);
+        }
+
+        #endregion
     }
 }
