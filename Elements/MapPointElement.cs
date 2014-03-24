713de2f804bdf420d7f086402f0743ebee6838b8
Evgeniy Plyusnin <eninbox@gmail.com>
2014-03-24 16:01:53 +0600

    MapPointElement теперь разрешает заменять Position, при этом запрашивая обновление визуального образа

--- Elements/MapPointElement.cs
+++ Elements/MapPointElement.cs
@@ -1,18 +1,32 @@
-﻿using System.Windows.Media;
+﻿using System.Windows;
+using System.Windows.Media;
 using Geographics;
 
 namespace MapVisualization.Elements
 {
     public abstract class MapPointElement : MapElement
     {
-        public MapPointElement(EarthPoint Position) { this.Position = Position; }
-        public EarthPoint Position { get; private set; }
+        private EarthPoint _position;
+        public MapPointElement(EarthPoint Position) { _position = Position; }
+
+        public EarthPoint Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                OnPositionChanged();
+            }
+        }
+
+        /// <summary>Возникает при изменении позиции точки</summary>
+        protected virtual void OnPositionChanged() { RequestChangeVisual(); }
 
         protected abstract void DrawPointElement(DrawingContext dc);
 
         protected override void Draw(DrawingContext dc, int Zoom)
         {
-            var elementPoint = Projector.Project(Position, Zoom);
+            Point elementPoint = Projector.Project(Position, Zoom);
             dc.PushTransform(new TranslateTransform(elementPoint.X, elementPoint.Y));
             DrawPointElement(dc);
             dc.Pop();
