86889ca1c463a28a008edd847c80cc8f340fa4b9
Evgeniy Plyusnin <eninbox@gmail.com>
2017-03-20 10:27:55 +0500

    Для класса MapPathElement добавлен метод GetScreenPoints, который позволяет оптимизировать количество отображаемых точек пути с учётом масштабирования при выводе на экран

--- Elements/MapPathElement.cs
+++ Elements/MapPathElement.cs
@@ -1,29 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using Geographics;
 
 namespace MapVisualization.Elements
 {
-    /// <summary>
-    /// Элемент карты, состоящий из нескольких точек
-    /// </summary>
+    /// <summary>Элемент карты, состоящий из нескольких точек</summary>
     public abstract class MapPathElement : MapElement
     {
-        protected virtual EarthArea ElementArea { get; private set; }
+        private readonly double _screenStepSquared;
 
-        /// <summary>
-        /// Создаёт новый многоточечный объект на карте
-        /// </summary>
+        /// <summary>Создаёт новый многоточечный объект на карте</summary>
         /// <param name="Points">Точки, входящие в состав объекта</param>
-        public MapPathElement(IList<EarthPoint> Points)
+        /// <param name="ScreenStep">Минимальная длинна сегмента для отрисовки на экране</param>
+        public MapPathElement(IList<EarthPoint> Points, double ScreenStep = 5)
         {
             this.Points = Points;
+            _screenStepSquared = Math.Pow(ScreenStep, 2);
             ElementArea = new EarthArea(Points.ToArray());
         }
-        /// <summary>
-        /// Точки, входящие в состав объекта
-        /// </summary>
-        public IList<EarthPoint> Points { get; private set; }
+
+        protected virtual EarthArea ElementArea { get; }
+
+        /// <summary>Точки, входящие в состав объекта</summary>
+        public IList<EarthPoint> Points { get; }
+
+        protected IEnumerable<Point> GetScreenPoints(int Zoom)
+        {
+            Point? previousPoint = null;
+            Point? tailPoint = null;
+            foreach (var point in Points)
+            {
+                tailPoint = Projector.Project(point, Zoom);
+                if (previousPoint == null || (previousPoint.Value - tailPoint.Value).LengthSquared >= _screenStepSquared)
+                {
+                    previousPoint = tailPoint;
+                    yield return tailPoint.Value;
+                }
+            }
+            if (previousPoint != null &&
+                tailPoint != previousPoint.Value)
+            {
+                yield return tailPoint.Value;
+            }
+        }
 
         /// <summary>Проверяет, попадает ли этот элемент в указанную области видимости</summary>
         /// <param name="VisibleArea">Область видимости</param>
